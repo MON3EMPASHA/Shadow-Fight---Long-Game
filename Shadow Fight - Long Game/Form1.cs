94e6d4dcf515aa5077452ecb9b931618ff2853a4
Abdelmonem Hatem <monemabdelmonem144@gmail.com>
2024-05-24 12:29:46 +0300

    Draw Hero

--- Shadow Fight - Long Game/Form1.cs
+++ Shadow Fight - Long Game/Form1.cs
@@ -8,11 +8,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+class Hero
+{
+    public int X, Y;
+    public Bitmap Img;
+    public Hero()
+    {
+        X = 100; Y=500;
+        Img = new Bitmap("Hero.png");
+    }
+}
+
 namespace Shadow_Fight___Long_Game
 {
     public partial class Form1 : Form
     {
+        //Bitmaps
         Bitmap off;
+        //Lists
+        List<Hero> heroList=new List<Hero>();
         public Form1()
         {
             WindowState= FormWindowState.Maximized;
@@ -34,15 +48,24 @@ namespace Shadow_Fight___Long_Game
         private void Form1_Load(object sender, EventArgs e)
         {
             off=new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            InitializeGame();
         }
         void InitializeGame()
         {
+            //Initializing when Pressing R
+            heroList.Clear();
+            // Creating Hero
+            Hero monem = new Hero(); heroList.Add(monem);
 
         }
         void DrawScene(Graphics g)
         {
-            g.Clear(Color.Black);
-            
+            g.Clear(Color.White);
+            //Hero
+            for(int i = 0; i < heroList.Count; i++)
+            {
+                g.DrawImage(heroList[i].Img, heroList[i].X, heroList[i].Y);
+            }
         }
         void DrawDubb(Graphics g)
         {
