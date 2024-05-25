9f691d1faa0ab0931850f64634326692fa589de6
Abdelmonem Hatem <monemabdelmonem144@gmail.com>
2024-05-25 03:36:05 +0300

    Added the kill animation and the health bar template

--- Shadow Fight - Long Game/Form1.cs
+++ Shadow Fight - Long Game/Form1.cs
@@ -10,35 +10,50 @@ using System.Windows.Forms;
 
 class Hero
 {
-    public int X, Y,iframe,speed,flagjump;
-    public List<Bitmap> ImgsR,ImgsL,ImgsJ;
+    public int X, Y,iframe,speed,flagjump,flagkill;
+    public List<Bitmap> ImgsR,ImgsL,ImgsJ,ImgsK1;
     public Hero()
     {
-        X = 100; Y=490; iframe = 0;speed = 30; flagjump = 0;
+        X = 100; Y=490; iframe = 0;speed = 30; flagjump = 0; flagkill = 0;
         //Right Movimg
-        ImgsR = new List<Bitmap>();
-        Bitmap monem = new Bitmap("HeroR.png");
-        ImgsR.Add(monem);
-        for (int i = 1; i < 15; i++)
         {
-            Bitmap monem2 = new Bitmap("herorunR/"+i+".png");
-            ImgsR.Add(monem2);
+            ImgsR = new List<Bitmap>();
+            Bitmap monem = new Bitmap("HeroR.png");
+            ImgsR.Add(monem);
+            for (int i = 1; i < 15; i++)
+            {
+                Bitmap monem2 = new Bitmap("herorunR/" + i + ".png");
+                ImgsR.Add(monem2);
+            }
         }
         //Left Moving
-        ImgsL = new List<Bitmap>();
-        monem = new Bitmap("HeroL.png");
-        ImgsL.Add(monem);
-        for (int i = 1; i < 15; i++)
         {
-            Bitmap monem2 = new Bitmap("herorunL/" + i + ".png");
-            ImgsL.Add(monem2);
+            ImgsL = new List<Bitmap>();
+            Bitmap monem = new Bitmap("HeroL.png");
+            ImgsL.Add(monem);
+            for (int i = 1; i < 15; i++)
+            {
+                Bitmap monem2 = new Bitmap("herorunL/" + i + ".png");
+                ImgsL.Add(monem2);
+            }
         }
         //jump
-        ImgsJ = new List<Bitmap>();
-        for (int i = 1; i < 15; i++)
         {
-            Bitmap monem2 = new Bitmap("jump/"+i+".png");
-            ImgsJ.Add(monem2);
+            ImgsJ = new List<Bitmap>();
+            for (int i = 1; i < 20; i++)
+            {
+                Bitmap monem2 = new Bitmap("jump/" + i + ".png");
+                ImgsJ.Add(monem2);
+            }
+        }
+        //Kill1
+        {
+            ImgsK1 = new List<Bitmap>();
+            for (int i = 2; i < 12; i++)
+            {
+                Bitmap monem2 = new Bitmap("kill1/k" + i + ".png");
+                ImgsK1.Add(monem2);
+            }
         }
     }
 }
@@ -63,7 +78,7 @@ namespace Shadow_Fight___Long_Game
         //Bitmaps
         Bitmap off;
         //Lists
-        List<Hero> heroList=new List<Hero>(); List<AdvancedImage> background = new List<AdvancedImage>();
+        List<Hero> heroList=new List<Hero>(); List<AdvancedImage> background = new List<AdvancedImage>(); 
         //Flags
         int flagheroleft = 0; Boolean flag_moving = false;
         //Timer
@@ -77,7 +92,7 @@ namespace Shadow_Fight___Long_Game
             this.Paint += Form1_Paint;
             this.KeyDown += Form1_KeyDown;
             this.KeyUp += Form1_KeyUp;
-            tt.Tick += Tt_Tick; tt.Interval = 100; tt.Start();
+            tt.Tick += Tt_Tick; tt.Interval = 50; tt.Start();
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
@@ -100,13 +115,21 @@ namespace Shadow_Fight___Long_Game
             {
                 if (ctjumptick == 1)
                 {
-                    if (heroList[0].iframe < 7) { heroList[0].Y -= heroList[0].speed; }
-                    else if (heroList[0].iframe >= 7) { heroList[0].Y += heroList[0].speed; }
-                    heroList[0].iframe++;
-                    if (heroList[0].iframe == 14) { heroList[0].iframe = 0; heroList[0].flagjump = 0; }
+                    if (heroList[0].iframe < 9) { heroList[0].Y -= heroList[0].speed; }
+                    else if (heroList[0].iframe >= 9) { heroList[0].Y += heroList[0].speed; }
+                    if (heroList[0].iframe == 16) { heroList[0].Y+= heroList[0].speed*3 ; }
+                    if (heroList[0].iframe == 18) { heroList[0].iframe = 0; heroList[0].flagjump = 0; flag_moving = false; heroList[0].Y -= heroList[0].speed * 3; }
                     ctjumptick = 0;
+                    heroList[0].iframe++;
                 }
                 ctjumptick++;
+            }
+            else if( heroList[0].flagkill == 1)
+            {
+                if (heroList[0].iframe == 5) { heroList[0].iframe = 0; heroList[0].flagkill = 0; heroList[0].Y = 490;}
+                heroList[0].iframe++;
+                heroList[0].Y = 525;
+                if (heroList[0].iframe >=3) { heroList[0].Y = 560; }
             }
             DrawDubb(CreateGraphics());
         }
@@ -115,7 +138,7 @@ namespace Shadow_Fight___Long_Game
             switch (e.KeyCode)
             {
                 case Keys.Left:
-                    if (heroList[0].flagjump == 0)
+                    if (heroList[0].flagjump == 0 && heroList[0].flagkill == 0)
                     {
                         heroList[0].iframe++;
                         heroList[0].X -= heroList[0].speed;
@@ -127,7 +150,7 @@ namespace Shadow_Fight___Long_Game
                     }
                     break;
                 case Keys.Right:
-                    if (heroList[0].flagjump == 0)
+                    if (heroList[0].flagjump == 0 && heroList[0].flagkill == 0)
                     {
                         heroList[0].iframe++;
                         heroList[0].X += heroList[0].speed;
@@ -139,14 +162,21 @@ namespace Shadow_Fight___Long_Game
                     }
                     break;
                 case Keys.Up:
-                    if (heroList[0].flagjump == 0)
+                    if (heroList[0].flagjump == 0 && heroList[0].flagkill==0)
                     {
                         heroList[0].flagjump = 1;
                         heroList[0].iframe = 0;
                         Tick();
                     }
                     break;
-            
+                case Keys.Space:
+                    if (heroList[0].flagjump == 0 && heroList[0].flagkill == 0)
+                    {
+                        heroList[0].flagkill = 1;
+                        heroList[0].iframe = 0;
+                        Tick();
+                    }
+                    break;
             }
             
         }
@@ -184,22 +214,28 @@ namespace Shadow_Fight___Long_Game
             //Hero
             for (int i = 0; i < heroList.Count; i++)
             {
-                if (flagheroleft == 1 && heroList[0].flagjump==0)
+                //Move Left
+                if (flagheroleft == 1 && heroList[0].flagjump==0 && heroList[0].flagkill == 0)
                 {
                     g.DrawImage(heroList[i].ImgsL[heroList[i].iframe], heroList[i].X, heroList[i].Y, heroList[i].ImgsL[heroList[i].iframe].Width*2, heroList[i].ImgsL[heroList[i].iframe].Height * 2);
                 }
-                else if (flagheroleft == 0 && heroList[0].flagjump == 0)
+                //Move Right 
+                else if (flagheroleft == 0 && heroList[0].flagjump == 0 && heroList[0].flagkill == 0)
                 {
                     g.DrawImage(heroList[i].ImgsR[heroList[i].iframe], heroList[i].X, heroList[i].Y, heroList[i].ImgsR[heroList[i].iframe].Width * 2, heroList[i].ImgsR [heroList[i].iframe].Height * 2);
                 }
-                else if(heroList[0].flagjump == 1)
+                else if(heroList[0].flagjump == 1 && heroList[0].flagkill == 0)
                 {
-            
                     g.DrawImage(heroList[i].ImgsJ[heroList[i].iframe], heroList[i].X, heroList[i].Y, heroList[i].ImgsJ[heroList[i].iframe].Width*4, heroList[i].ImgsJ[heroList[i].iframe].Height*4);
-
                 }
-
+                else if (heroList[0].flagkill == 1 && heroList[0].flagjump == 0)
+                {
+                    g.DrawImage(heroList[i].ImgsK1[heroList[i].iframe], heroList[i].X, heroList[i].Y, heroList[i].ImgsK1[heroList[i].iframe].Width * 4, heroList[i].ImgsK1[heroList[i].iframe].Height * 4);
+                }
             }
+            //Health Bar
+            g.DrawImage(new Bitmap("heroHealthBar.png"), 1, 0);
+        
         }
         void DrawDubb(Graphics g)
         {
